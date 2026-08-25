a7d024adf41944a1aee6039dee9c48ab483b2a99
Jhonatan David Hostia Arrieta <101542114+jhostia@users.noreply.github.com>
2026-08-25 15:49:37 -0500

    Agregue el panel del libro

---
+++ Assets/Story/Book/Script/BookController.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BookController : MonoBehaviour
+{
+    [Header("Interfaz")]
+    public GameObject bookPanel;
+    public TMP_Text pageText;
+    public TMP_Text pageNumberText;
+
+    [Header("Botones")]
+    public Button previousButton;
+    public Button nextButton;
+
+    [Header("Contenido del libro")]
+    public TextAsset bookText;
+
+    private string[] pages;
+    private int currentPage = 0;
+
+    private void Start()
+    {
+        LoadPages();
+        CloseBook();
+    }
+
+    private void LoadPages()
+    {
+        if (bookText == null)
+        {
+            Debug.LogError("BookController: No se asignó el archivo del libro.");
+            return;
+        }
+
+        pages = bookText.text.Split(
+            new string[] { "[[PAGE]]" },
+            System.StringSplitOptions.RemoveEmptyEntries
+        );
+
+        for (int i = 0; i < pages.Length; i++)
+        {
+            pages[i] = pages[i].Trim();
+        }
+    }
+
+    public void OpenBook()
+    {
+        currentPage = 0;
+
+        bookPanel.SetActive(true);
+
+        ShowPage();
+    }
+
+    public void CloseBook()
+    {
+        bookPanel.SetActive(false);
+    }
+
+    public void NextPage()
+    {
+        if (pages == null)
+            return;
+
+        if (currentPage < pages.Length - 1)
+        {
+            currentPage++;
+            ShowPage();
+        }
+    }
+
+    public void PreviousPage()
+    {
+        if (pages == null)
+            return;
+
+        if (currentPage > 0)
+        {
+            currentPage--;
+            ShowPage();
+        }
+    }
+
+    private void ShowPage()
+    {
+        if (pages == null || pages.Length == 0)
+        {
+            pageText.text = "El libro no contiene páginas.";
+            return;
+        }
+
+        pageText.text = pages[currentPage];
+
+        if (pageNumberText != null)
+        {
+            pageNumberText.text =
+                "Página " + (currentPage + 1) + " de " + pages.Length;
+        }
+
+        if (previousButton != null)
+        {
+            previousButton.interactable = currentPage > 0;
+        }
+
+        if (nextButton != null)
+        {
+            nextButton.interactable = currentPage < pages.Length - 1;
+        }
+    }
+}
